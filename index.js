87efff084fcf00dfb3f667738c04f3739a9180c5
Tarun Dumka <tarun.dumka@nagarro.com>
2023-11-30 23:52:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = 4000  
+
+app.get('/', (req, res) => {
+  res.send('tarundumka')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.get('/login', (req, res) => {
+    res.send('<h1>please login at chai or code <h/>')
+  })
+
+app.get('/youtube', (req, res) => {
+    res.send('<h1><h/>')
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
